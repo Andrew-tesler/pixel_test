f399714427704ce2cb195d6e698913eae4f33481
Andrew Tesler <tesler.andrew@gmail.com>
2022-07-19 14:40:32 +0300

    Added more define RGB to the list of colors

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # from machine import Pin
 # from adafruit_pixelbuf import PixelBuf
 from neopixel import NeoPixel
-from machine import Pin
+from machine import Pin, ADC
 from time import sleep
 from random import *
 from urandom import *
@@ -15,6 +15,8 @@ NEOPIXEL_PIN    = 2       # GPIO pin connected to the pixels (must support PWM!)
 BUTTON_PIN      = 6       # GPIO pin connected to button
 PixelBuf        = 3       # Number for each pixel
 
+MIN_COLOR      = 0       # Minimum color value
+MAX_COLOR      = 5      # Maximum color value
 # init function for the LED strip and button
 NEOPIXEL_PIN   = Pin(NEOPIXEL_PIN, Pin.OUT)
 BUTTON_PIN     = Pin(BUTTON_PIN, Pin.IN, Pin.PULL_UP)
@@ -23,9 +25,9 @@ BUTTON_PIN     = Pin(BUTTON_PIN, Pin.IN, Pin.PULL_UP)
 STRIP_ARRAY = [[0 for x in range(3)] for y in range(LED_COUNT)]
 
 # Init the LED strip
-strip = NeoPixel(NEOPIXEL_PIN, LED_COUNT)
+strip = NeoPixel(NEOPIXEL_PIN, LED_COUNT, timing=1)
 
-# print(STRIP_ARRAY)
+print(STRIP_ARRAY)
 # run over the LED strip and set the color to black
 for i in range(LED_COUNT):
     strip[i] = (0, 0, 0)
@@ -37,12 +39,19 @@ print("Strip length: ", strip.n)
 print("Strip pin: ", strip.pin)
 print("Strip pixel count: ", strip.n)
 
+adc = ADC(Pin(26))     # create ADC object on ADC pin
+adc.read_u16()         # read value, 0-65535 across voltage range 0.0v - 3.3v
+print("Analog input pin: ", adc.read_u16() )
 
 # # Testing the LED strip
 # STRIP_ARRAY[2] = [125, 0, 10]
 
 # strip[-1] = STRIP_ARRAY[2]
 # np[pixel] = (i, i, i)
+# Generate a random color with different values for each color
+# R = randint(0, 20)
+# G = randint(0, 20)
+# B = randint(0, 20)
 
 # main loop
 while True:
@@ -50,21 +59,30 @@ while True:
     if BUTTON_PIN.value() == 0:
         # set all leds to random color
         for i in range(LED_COUNT):
-            strip[i] = (randint(0, 20), randint(5, 20), randint(0, 50))
+            STRIP_ARRAY[i][0] = randint(MIN_COLOR, MAX_COLOR)
+            STRIP_ARRAY[i][1] = randint(MIN_COLOR, MAX_COLOR - STRIP_ARRAY[i][0])
+            STRIP_ARRAY[i][2] = randint(MIN_COLOR, MAX_COLOR - STRIP_ARRAY[i][1])
+            strip[i] = (STRIP_ARRAY[i])
             
             # sleep(0.01)
     # check if the button is not pressed
     else:
         # set all leds to Blue
         for i in range(LED_COUNT):
-            strip[i] = (0, 5, 5)
+            strip[i] = (2, 0, 1)
            
             # sleep(0.01)
     # Update the LED strip with the new values from the array
     # for i in range(LED_COUNT):
         # strip[i] = STRIP_ARRAY[i]
-    
-    strip.write()
+    # print strip RGB values
+    # print(strip.rgb)
+
+    # print(strip.n)
+    # get and print analog value of A1 pin
+    # print(adc.read())
+    # print("Analog input pin: ", adc.read_u16() )
+    # strip.write()
 
     # strip.write()
 # strip[2] = (200,0,0)
