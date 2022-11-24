316a0a31e2c5687fb94478f1f07c0c1de38e5378
Andrew Tesler <tesler.andrew@gmail.com>
2022-11-24 22:50:36 +0200

    Testing LEDS on mushroom device

--- main.py
+++ main.py
@@ -7,90 +7,195 @@ from random import *
 from urandom import *
 
 
-LED_COUNT = 4
+LED_COUNT = 24
 
 print('Testing')
 
 import random
-x = random.getrandbits(32)
+x = random.getrandbits(24)
 print("{}".format(x))
 
 pin = Pin(4, Pin.OUT)   # set GPIO0 to output to drive NeoPixels
 np = NeoPixel(pin, LED_COUNT)   # create NeoPixel driver on GPIO0 for 4 pixels
 
-np[0] = (0, 0, 0)
-np[1] = (12, 0, 50)
-np[2] = (0, 12, 0)
-np[3] = (0, 0, 50)
-np.write()
-# sleep(3)
-pixel = [0, 1, 2]      # set first pixel to black
+# np[0] = (0, 0, 0)
+# np[1] = (12, 0, 50)
+# np[2] = (0, 12, 0)
+# np[3] = (0, 0, 50)
+# np.write()
+# # sleep(3)
+# pixel = [0, 1, 2]      # set first pixel to black
+
+MAX_BRIGHTNESS = 255
+TRANSITION_TIME = 0.01
 
 
-def randrange(start, stop=None):
-    if stop is None:
-        stop = start
-        start = 0
-    upper = stop - start
-    bits = 0
-    pwr2 = 1
-    while upper > pwr2:
-        pwr2 <<= 1
-        bits += 1
-    while True:
-        r = getrandbits(bits)
-        if r < upper:
-            break
-    return r + start
+def set_color(np, pixel, color):
+    np[pixel] = color
+    np.write()
+
+def set_strip_color(np, color):
+    for i in range(0, LED_COUNT):
+        np[i] = color
+    np.write()
 
 
-def randint(start, stop):
-    return randrange(start, stop + 1)
 
-# fade function from Adafruit NeoPixel library
-def fade(np, pixel, brightness):
-    """Fade the given pixel in and out."""
-    for i in range(0, 255):
-        np[pixel] = (i, i, i)
+# create fade in/out effect for color change
+def fade_in_out(np, pixel, color, steps, delay):
+    for i in range(0, steps):
+        np[pixel] = (int(color[0] * i / steps), int(color[1] * i / steps), int(color[2] * i / steps))
         np.write()
-        sleep(0.01)
-    for i in range(255, 0, -1):
-        np[pixel] = (i, i, i)
+        sleep(delay)
+    for i in range(steps, 0, -1):
+        np[pixel] = (int(color[0] * i / steps), int(color[1] * i / steps), int(color[2] * i / steps))
         np.write()
-        sleep(0.01)
+        sleep(delay)
 
-# blink all leds at random intervals and random colours
+
+def fade_in(np, pixel, color, steps, delay):
+    for i in range(0, steps):
+        np[pixel] = (int(color[0] * i / steps), int(color[1] * i / steps), int(color[2] * i / steps))
+        np.write()
+        sleep(delay)
+
+def fade_out(np, pixel, color, steps, delay):
+    for i in range(steps, 0, -1):
+        np[pixel] = (int(color[0] * i / steps), int(color[1] * i / steps), int(color[2] * i / steps))
+        np.write()
+        sleep(delay)
+
+
+set_strip_color(np, (0, 0, 10))
+sleep(1)
+set_strip_color(np, (0, 10, 0))
+sleep(1)
+set_strip_color(np, (10, 0, 0))
+sleep(1)
+
 while True:
-    for i in range(LED_COUNT):
-        np[i] = (randint(0, 125), randint(0, 125), randint(0, 125))
-        np.write()
+    for i in range(0, 24):
+        fade_in(np, i, (0, 0, MAX_BRIGHTNESS), 10, TRANSITION_TIME)
+        sleep(0.1)
+    
+    for i in range(0, 24):
+        fade_out(np, i, (0, 0, MAX_BRIGHTNESS), 10, TRANSITION_TIME)
+        sleep(0.1)
+
+    for i in range(0, 24):
+        fade_in(np, i, (0, MAX_BRIGHTNESS, 0), 10, TRANSITION_TIME)
+        sleep(0.1)
+
+    for i in range(0, 24):
+        fade_out(np, i, (0, MAX_BRIGHTNESS, 0), 10, TRANSITION_TIME)
+        sleep(0.1)
+
+    for i in range(0, 24):
+        fade_in(np, i, (MAX_BRIGHTNESS, 0, 0), 10, TRANSITION_TIME)
+        sleep(0.1)
+
+    for i in range(0, 24):
+        fade_out(np, i, (MAX_BRIGHTNESS, 0, 0), 10, TRANSITION_TIME)
+        sleep(0.1)
+
+    for i in range(0, 24):
+        fade_in(np, i, (MAX_BRIGHTNESS, 0, MAX_BRIGHTNESS), 10, TRANSITION_TIME)
+        sleep(0.1)
+
+    for i in range(0, 24):
+        fade_out(np, i, (MAX_BRIGHTNESS, 0, MAX_BRIGHTNESS), 10, TRANSITION_TIME)
+        sleep(0.1)
+
+    for i in range(0, 24):
+        fade_in(np, i, (MAX_BRIGHTNESS, MAX_BRIGHTNESS, 0), 10, TRANSITION_TIME)
+        sleep(0.1)
+
+    for i in range(0, 24):
+        fade_out(np, i, (MAX_BRIGHTNESS, MAX_BRIGHTNESS, 0), 10, TRANSITION_TIME)
+        sleep(0.1)
+
+    for i in range(0, 24):
+        fade_in(np, i, (0, MAX_BRIGHTNESS, MAX_BRIGHTNESS), 10, TRANSITION_TIME)
+        sleep(0.1)
+    for i in range(0, 24):
+        fade_out(np, i, (0, MAX_BRIGHTNESS, MAX_BRIGHTNESS), 10, TRANSITION_TIME)
+        sleep(0.1)
+    for i in range(0, 24):
+        fade_in(np, i, (MAX_BRIGHTNESS, MAX_BRIGHTNESS, MAX_BRIGHTNESS), 10, TRANSITION_TIME)
+        sleep(0.1)
+    for i in range(0, 24):
+        fade_out(np, i, (MAX_BRIGHTNESS, MAX_BRIGHTNESS, MAX_BRIGHTNESS), 10, TRANSITION_TIME)
+        sleep(0.1)
+
+
+# def randrange(start, stop=None):
+#     if stop is None:
+#         stop = start
+#         start = 0
+#     upper = stop - start
+#     bits = 0
+#     pwr2 = 1
+#     while upper > pwr2:
+#         pwr2 <<= 1
+#         bits += 1
+#     while True:
+#         r = getrandbits(bits)
+#         if r < upper:
+#             break
+#     return r + start
+
+
+# def randint(start, stop):
+#     return randrange(start, stop + 1)
+
+# # fade function from Adafruit NeoPixel library
+# def fade(np, pixel, brightness):
+#     """Fade the given pixel in and out."""
+#     for i in range(0, 255):
+#         np[pixel] = (i, i, i)
+#         np.write()
+#         sleep(0.01)
+#     for i in range(255, 0, -1):
+#         np[pixel] = (i, i, i)
+#         np.write()
+#         sleep(0.01)
+
+# pixel_r = (randint(0, 25), randint(0, 125), randint(0, 125))
+# # blink all leds at random intervals and random colours
+# while True:
+    # set_color(np, pixel[0], pixel_r)
+
+#     # pixel_r = (randint(0, 250), randint(0, 250), randint(0, 250))
+#     # for i in range(LED_COUNT):
+#     #     np[i] = pixel_r
+#     #     np.write()
         
-        # sleep(0.1)
-    sleep(randint(1, 3))   
+#     sleep(0.1)
+#     # sleep(randint(1, 3))   
 
 
-while True:
-    p = 0
-    for p in range(0, 3):
-        for n in range(10, 255):
-            pixel[p] = n
-            np[0] = pixel
-            np[1] = pixel
-            np[2] = pixel
-            np[3] = pixel
-            np.write()
-            sleep(0.1)
-        for n in range(10, 255):
-            pixel[p] = 255 - n
-            np[0] = pixel
-            np[1] = pixel
-            np[2] = pixel
-            np[3] = pixel
-            np.write()
-            sleep(0.1)
+# while True:
+#     p = 0
+#     for p in range(0, 3):
+#         for n in range(10, 255):
+#             pixel[p] = n
+#             np[0] = pixel
+#             np[1] = pixel
+#             np[2] = pixel
+#             np[3] = pixel
+#             np.write()
+#             sleep(0.1)
+#         for n in range(10, 255):
+#             pixel[p] = 255 - n
+#             np[0] = pixel
+#             np[1] = pixel
+#             np[2] = pixel
+#             np[3] = pixel
+#             np.write()
+#             sleep(0.1)
 
 
-# for p in pixel:
+# # for p in pixel:
 #     print(p)
 #     for i in range(10, 255):
 #         np[0] = (i, 0, 0)
